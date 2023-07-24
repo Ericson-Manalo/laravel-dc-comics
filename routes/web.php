9f38745cc3b2aa9e0c47efbff2604d2679025f2b
Ericson-Manalo <ericmana98@gmail.com>
2023-07-24 15:18:04 +0200

    used the route function to connect with the controller created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ComicController as AdminComicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('admin/comics', [AdminComicController::class, 'index'])->name('admin.comics.index');
