de3676a8a17f2b25fb938659f0075397b270fba2
Ericson-Manalo <ericmana98@gmail.com>
2023-07-24 20:43:41 +0200

    renamed my array

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
+
 
 class ComicSeeder extends Seeder
 {
@@ -15,9 +17,7 @@ class ComicSeeder extends Seeder
     public function run()
     {
         //
-
-        return [
-            'comicList'=>[
+        $comicList=[
             [
         
                 "title" => "Action Comics #1000: The Deluxe Edition",
@@ -420,14 +420,20 @@ class ComicSeeder extends Seeder
                 ],
         
             ],
-        ]
-        
         ];
+
 
         foreach ($comicList as $comic){
             $newComic = new Comic();
             $newComic->title = $comic['title'];
-            $newComic->save(); 
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->type = $comic['type'];
+
+
+            $newComic->save();
 
         }
     }
