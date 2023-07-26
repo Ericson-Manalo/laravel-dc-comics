cb733413aebb643daa5c70592a8175e70025a73c
Ericson-Manalo <ericmana98@gmail.com>
2023-07-26 17:28:37 +0200

    created a route put into the image to back in the homepage of the app

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,8 @@
 <header>
     <div class="header-section">
-        <img src="{{ Vite::asset ('resources/img/dc-logo.png') }}" alt="Dc Main Logo">
+        <a href="{{route('admin.comics.index')}}">
+            <img src="{{ Vite::asset ('resources/img/dc-logo.png') }}" alt="Dc Main Logo">
+        </a>
         <nav class="nav-bar">
             <ul>
             @foreach ($navbarLinks as $link)
